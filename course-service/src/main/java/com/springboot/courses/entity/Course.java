ba240402cb9913f6bf7356bda7bae0b14f4a72f0
karthikshankar1101 <karthikshankar1101@gmail.com>
2023-08-13 17:10:26 +0530

    Changes made to the pom.xml of all the services to match the version compatability

--- course-service/src/main/java/com/springboot/courses/entity/Course.java
+++ course-service/src/main/java/com/springboot/courses/entity/Course.java
@@ -1,10 +1,10 @@
 package com.springboot.courses.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
