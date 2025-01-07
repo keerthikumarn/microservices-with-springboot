38f5edebcd77af1856befec6f72a8a55008467a8
Keerthi Kumar <n.keerthikumar85@gmail.com>
2025-01-07 12:24:09 +0530

    changes as per JDK 21

--- course-service/src/main/java/com/springboot/courses/CourseServiceApplication.java
+++ course-service/src/main/java/com/springboot/courses/CourseServiceApplication.java
@@ -3,10 +3,8 @@ package com.springboot.courses;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 @EnableDiscoveryClient
 
 public class CourseServiceApplication {
